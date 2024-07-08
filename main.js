57eacc200715a116c282bf3f0dcfa817ea74fb60
mohamed-yesser <yesser305@gmail.com>
2024-07-08 19:09:17 +0100

    v3

--- main.js
+++ main.js
@@ -21,25 +21,29 @@ function generateID (){
 var colsAndRows =
 
 
-[[0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0]
+[[0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0]
 ]
+
+
+
 
 
 
 var gridContainer = $('#grid-container')
 
-colsAndRows.forEach((row, rowIndex) => {
+colsAndRows.forEach((row, colIndex) => {
     var rowElement = $('<div  class="grid-row"></div>')
 
-    row.forEach((col, colIndex) => {
-        var gridItem = $(`<div class="grid-item" id=" ${rowIndex}-${colIndex}">row ${rowIndex}-${colIndex}</div>`)
+    row.forEach((col, rowIndex) => {
+        var gridItem = $(`<div class="grid-item" id="${rowIndex}-${colIndex}">row ${rowIndex}-${colIndex}</div>`)
         rowElement.append(gridItem)
     });
 
@@ -50,21 +54,107 @@ colsAndRows.forEach((row, rowIndex) => {
   
 
 
-$('.grid-item').click(function(event) {
 
 
-var z =  $(this).attr('id')
 
-       
-        $(this).css('background-color', 'black')
-        console.log('white')
-    
-console.log(z)
-
-
+function toInt(el) {
+    return el.map(function(e) {
+        return parseInt(e)
+    })
 }
-)
 
+
+
+
+
+
+// $('.grid-item').click(function() {
+
+//     // console.log($(this).css('background-color'));
+
+
+
+//     var id = $(this).attr('id');
+//     var coordinates = id.split('-')
+    
+//     currentElement.push(toInt(coordinates))
+//     console.log(currentElement)
+
+
+    
+
+
+
+    
+// var color = $(this).css('background-color')
+
+// if (color === 'rgb(255, 255, 255)'){
+//     $(this).css('background-color', 'black')
+
+// }
+
+// else {
+//     $(this).css('background-color', 'rgb(255, 255, 255)');
+// }}
+
+
+
+
+// )
+
+
+
+
+var currentElement = [];
+
+$('.grid-item').click(function() {
+    var id = $(this).attr('id');
+    var coordinates = toInt(id.split('-'))
+
+    var row = coordinates[0];
+    var col = coordinates[1];
+
+
+
+ if (colsAndRows[row][col] === 0) {
+       
+        colsAndRows[row][col] = 1
+        $(this).css('background-color', 'black')
+        
+    
+        currentElement.push(coordinates)
+    } else {
+       
+        colsAndRows[row][col] = 0
+        $(this).css('background-color', 'rgb(255, 255, 255)');
+        
+        
+        currentElement = currentElement.filter(function(coord) {
+            return !(coord[0] === row && coord[1] === col)
+        });
+    }
+
+    
+    console.log('matrix ', colsAndRows);
+    console.log(currentElement)
+});
+
+
+
+
+
+
+
+// $('#add').click(function(form){
+
+
+
+    
+
+
+
+
+// })
 
 
 
